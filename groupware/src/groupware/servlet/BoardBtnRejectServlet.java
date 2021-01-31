ada93a0ec2e114d375f05103b5d267c39a3172e7
pc <pc@DESKTOP-U2GSG64>
2021-01-31 14:46:59 +0900

    결재반려 추가 -끝-

---
+++ groupware/src/groupware/servlet/BoardBtnRejectServlet.java
@@ -0,0 +1,31 @@
+package groupware.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import groupware.beans.BoardPaymentDao;
+@WebServlet(urlPatterns = "/board/payment_btn_reject.do")
+public class BoardBtnRejectServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			req.setCharacterEncoding("UTF-8");
+			
+			int payment_no = Integer.parseInt(req.getParameter("payment_no"));
+			BoardPaymentDao dao = new BoardPaymentDao();
+			dao.reject(payment_no);
+			
+			resp.sendRedirect("payment_list.jsp");
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
